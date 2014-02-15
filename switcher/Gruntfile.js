cd45b1289ad500c742c95dadce641198786d50c6
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2014-02-15 15:42:56 +0400

    more changes

--- switcher/Gruntfile.js
+++ switcher/Gruntfile.js
@@ -1,9 +1,37 @@
 module.exports = function(grunt) {
+  'use strict';
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    jslint: {
+      client: {
+        src: [
+          '*.js'
+        ],
+        directives: {
+          browser: true,
+          predef: [
+            'jQuery'
+          ]
+        },
+      }
+    },
+
+    watch: {
+      src: {
+        files: ['*.js', '*.сss'],
+        options: {
+          livereload: true,
+        },
+      }
+    }
   });
 
+  // Default task(s).
+ 
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jslint');
+  grunt.registerTask('default', 'jslint');
 
 };
